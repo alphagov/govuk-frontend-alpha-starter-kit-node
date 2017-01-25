4ac044108d28daeecca38aaef957cb487d288a5e
Gemma Leigh <hello@gemmaleigh.com>
2017-01-25 15:01:26 +0000

    Amend compilation task to output minified css
    - Add gulp-rename as a requirement

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@
 
 var paths = require('./config/paths.json')
 var gulp = require('gulp')
+var gutil = require('gulp-util')
 var sass = require('gulp-sass')
+var rename = require('gulp-rename')
 
 gulp.task('default', function () {
   console.log('Default task')
 })
 
-// Compile Sass to CSS
+// Styles build task ---------------------
+// Compiles CSS from Sass
+// Output both a minified and non-minified version into /public/css.
+// ---------------------------------------
+
 gulp.task('styles', function () {
   gulp.src(paths.assetsScss + '**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.publicCss))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.publicCss))
 })
