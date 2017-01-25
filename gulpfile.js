dd59271651e0995d6f18cea99e821e8a5765b6fa
Gemma Leigh <hello@gemmaleigh.com>
2017-01-25 14:47:26 +0000

    Add the gulp-sass package and compile scss to css
    - Add instructions for setup to the README
    - Add gulp-sass as a dependency
    - Add a gulp styles task to compile scss to css
    - Add a config file for application paths
    - Define the $path for image assets using the url-helpers function in
    the scss files
    - Add the /public folder to the gitignore file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
+'use strict'
+
+var paths = require('./config/paths.json')
 var gulp = require('gulp')
+var sass = require('gulp-sass')
 
 gulp.task('default', function () {
   console.log('Default task')
+})
+
+// Compile Sass to CSS
+gulp.task('styles', function () {
+  gulp.src(paths.assetsScss + '**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.publicCss))
 })
